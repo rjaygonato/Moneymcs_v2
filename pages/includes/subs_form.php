9bf32b7d81516337dab4f93d050e8b744d423492
RJ Gonato <53969477+rjaygonato@users.noreply.github.com>
2022-11-18 02:13:51 +0800

    Updates

--- pages/includes/subs_form.php
+++ pages/includes/subs_form.php
@@ -111,7 +111,7 @@
   <div class="modal-dialog modal-sm" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel2">Subscription</h5>
+        <h5 class="modal-title" id="exampleModalLabel2">Pay with Paypal</h5>
         <button
           type="button"
           class="btn-close"
