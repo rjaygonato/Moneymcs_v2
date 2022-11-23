99c88b6c8d4a8ee9930882215ca76dc651aac37c
RJ Gonato <53969477+rjaygonato@users.noreply.github.com>
2022-11-24 02:50:47 +0800

    Updates

--- pages/clients.php
+++ pages/clients.php
@@ -66,8 +66,127 @@
               <i class="bx bx-chevron-left bx-sm align-middle"></i>
             </a>
           </div>
+          <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">Main Menu</span>
+          </li>
+          <ul class="menu-inner py-1">
+            <!-- Dashboard -->
+            <li class="menu-item ">
+              <a href="home" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Home</div>
+              </a>
+            </li>
 
-          <?php include 'includes/resource_sidemenu.php'; ?>
+            <li class="menu-item ">
+              <a href="subscriptions" class="menu-link">
+                <i class='menu-icon tf-icons bx bx-list-ul'></i>
+                <div data-i18n="Analytics">Subscription Plans</div>
+              </a>
+            </li>
+
+            <!-- Layouts -->
+            <li class="menu-item" style="">
+              <a href="javascript:void(0)" class="menu-link menu-toggle">
+              <i class='menu-icon tf-icons bx bx-book-reader'></i>
+                <div data-i18n="">Partner Resources</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="resources" class="menu-link">
+                    <div data-i18n="">Resource Library</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="resource" class="menu-link">
+                    <div data-i18n="">Resource Page</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li class="menu-item">
+              <a href="javascript:void(0)" class="menu-link menu-toggle">
+                <i class='menu-icon tf-icons bx bxs-user-detail'></i>
+                <div data-i18n="">Commissions/Referrals</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="#" class="menu-link">
+                    <div data-i18n="">My Programs</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">
+                    <div data-i18n="">Link Generator</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">
+                    <div data-i18n="">Email Templates</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">
+                    <div data-i18n="">Banners</div>
+                    </a>
+                </li>
+              </ul>
+            </li>
+
+            <li class="menu-item active">
+              <a href="clients" class="menu-link">
+                <i class='menu-icon tf-icons bx bxs-user-rectangle'></i>
+                <div data-i18n="Basic">Clients</div>
+              </a>
+            </li>
+
+            <!-- <li class="menu-item">
+              <a href="#" class="menu-link">
+                <i class='menu-icon tf-icons bx bxs-user-plus'></i>
+                <div data-i18n="Basic">Add Clients</div>
+              </a>
+            </li> -->
+
+            <li class="menu-item ">
+              <a href="downlines" class="menu-link">
+                <i class='menu-icon tf-icons bx bx-git-branch'></i>
+                <div data-i18n="Basic">Downlines</div>
+              </a>
+            </li>
+
+            <li class="menu-item">
+              <a href="contacts" class="menu-link">
+                <i class='menu-icon tf-icons bx bx-phone-outgoing'></i>
+                <div data-i18n="Basic">Contact Us</div>
+              </a>
+            </li>
+
+          <!-- <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">Reports</span>
+          </li>
+          
+          <li class="menu-item">
+              <a href="" class="menu-link">
+              <i class='menu-icon tf-icons bx bx-receipt'></i>
+                <div data-i18n="Basic">Ledger</div>
+              </a>
+            </li>
+
+            <li class="menu-item">
+              <a href="" class="menu-link">
+                <i class='menu-icon tf-icons bx bx-current-location'></i>
+                <div data-i18n="Basic">Link Tracking Stats</div>
+              </a>
+            </li>
+
+            <li class="menu-item">
+              <a href="" class="menu-link">
+                <i class='menu-icon tf-icons bx bx-money'></i>
+                <div data-i18n="Basic">Commissions Generated</div>
+              </a>
+            </li> -->
+           
           </ul>
         </aside>
         <!-- / Menu -->
@@ -208,12 +327,13 @@
                             <div class="table-responsive text-nowrap">
                               <table id="" class="table table-borderless">
                                 <thead>
-                                  <th>Company </th>
-                                  <th>Name </th> 
-                                  <th>Contact </th>
-                                  <th>Email </th>
-                                  <th>Date Added </th>
-                                  <th>Actions</th>
+                                  <!-- <th>Company </th> -->
+                                  <th> Name </th> 
+                                  <th> Email </th>
+                                  <th> Contact </th>
+                                  <th> Type </th>
+                                  <th> Date Added </th>
+                                  <th> Actions</th>
                                 </thead>
                                 <tbody>
                                   <?php
@@ -226,7 +346,14 @@
 
                                         $status = ($row['status']) ? '<span class="badge rounded-pill bg-label-success">Active</span>' : '<span class="badge bg-label-secondary">Inactive</span>';
                                         echo "
-                                          
+                                          <tr>
+                                            <td>".$row['firstname'].",".$row['firstname']."</td>
+                                            <td>".$row['email']."</td>
+                                            <td>".$row['phonenum']."</td>
+                                            <td>".$row['type']."</td>
+                                            <td>".$row['date_join']."</td>
+                                            <td></td>
+                                          </tr>
                                         ";
                                       }
                                     }
