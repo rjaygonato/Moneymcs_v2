5d9b12cb8b6c7d29c1267076ab2738d9dae9caa3
RJ Gonato <53969477+rjaygonato@users.noreply.github.com>
2022-12-01 16:59:16 +0800

    Updates

--- pages/payment.php
+++ pages/payment.php
@@ -297,12 +297,7 @@
                         <span class="align-middle">Payment</span>
                       </a>
                     </li>
-                    <li>
-                      <a class="dropdown-item" href="payment">
-                      <i class='bx bx-dollar-circle me-2'></i>
-                        <span class="align-middle">Payment</span>
-                      </a>
-                    </li>
+                    
                     <!-- <li>
                       <a class="dropdown-item" href="transactions">
                         <i class='bx bx-folder-open'></i>
