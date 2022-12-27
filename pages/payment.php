fada4528c6ff55c5a9fa3ec9a2fb8527777b4966
RJ Gonato <53969477+rjaygonato@users.noreply.github.com>
2022-12-27 12:13:38 +0800

    Updates

--- pages/payment.php
+++ pages/payment.php
@@ -481,10 +481,10 @@
             <footer class="content-footer footer bg-footer-theme">
               <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
                 <div class="mb-2 mb-md-0">
-                  <!-- ©
+                  ©
                   <script>
                     document.write(new Date().getFullYear());
-                  </script> -->
+                  </script> Money MCS
                   <!-- , made with ❤️ by -->
                   <!-- <a href="#" target="_blank" class="footer-link fw-bolder"></a> -->
                 </div>
